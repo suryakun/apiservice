49f9dd07f612c8ad0809fb14ac170ec31c56bc81
surya <surya.ramshere@gmail.com>
2015-10-24 15:19:10 +0700

    fixing config

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -6,7 +6,7 @@ module.exports = {
   // Server IP
   ip:       process.env.OPENSHIFT_NODEJS_IP ||
             process.env.IP ||
-            111.221.104.74,
+            0.0.0.0,
 
   // Server port
   port:     process.env.OPENSHIFT_NODEJS_PORT ||

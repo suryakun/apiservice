b6b88c81a6b3d981202964b9e76299f9afd8f0ea
surya <surya.ramshere@gmail.com>
2015-10-24 15:22:30 +0700

    fixing config

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -6,7 +6,7 @@ module.exports = {
   // Server IP
   ip:       process.env.OPENSHIFT_NODEJS_IP ||
             process.env.IP ||
-            'localhost',
+            'apiservice7pagi',
 
   // Server port
   port:     process.env.OPENSHIFT_NODEJS_PORT ||

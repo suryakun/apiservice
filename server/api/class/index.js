461f52f6b0e21a059fe5c9d72e131a1780bdc823
surya <surya.ramshere@gmail.com>
2015-10-26 20:27:59 +0700

    fixing error api

--- server/api/class/index.js
+++ server/api/class/index.js
@@ -6,7 +6,7 @@ var controller = require('./class.controller');
 var router = express.Router();
 
 router.get('/', controller.index);
-router.get('/:id', controller.show);
+router.get('/single/:id', controller.show);
 router.post('/', controller.create);
 router.put('/:id', controller.update);
 router.patch('/:id', controller.update);

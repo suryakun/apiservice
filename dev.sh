4ff5348f100101d63949c48172ade930027be0d7
Dede Hidayat <dede.hidayat@wgs.co.id>
2016-03-16 22:13:06 +0700

    update bash env

--- dev.sh
+++ dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Export environment for development"
 
-export DOMAIN='http://localhost:9000'
+export DOMAIN='http://7pagi.dev:9000'
 export SESSION_SECRET='room-secret'
 
 export FACEBOOK_ID='app-id'
@@ -12,3 +12,6 @@ export TWITTER_SECRET='secret'
 
 export GOOGLE_ID='app-id'
 export GOOGLE_SECRET='secret'
+
+export AZURE_ID='d081090e-7e9f-4e95-8983-187e6e2fe264'
+export AZURE_SECRET='q049Mvl4RbFxLtYxP0eYYUbdYAre7s3v5Aw/gA52RiQ='

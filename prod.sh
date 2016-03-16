8780be3c899fdc966f90e2a0777ef6f252d2d045
surya <surya@localhost.localdomain>
2016-03-17 00:00:52 +0700

    update local from code dede

--- prod.sh
+++ prod.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Export environment for production"
 
-export DOMAIN='http://localhost:9000'
+export DOMAIN='http://web.7pagi.com:8080'
 export SESSION_SECRET='room-secret'
 
 export FACEBOOK_ID='app-id'
@@ -12,3 +12,6 @@ export TWITTER_SECRET='secret'
 
 export GOOGLE_ID='app-id'
 export GOOGLE_SECRET='secret'
+
+export AZURE_ID='6ba57872-459a-4647-9022-0c4a78800fdb'
+export AZURE_SECRET='oISP3dRBHjV8lfWoXDmihFf1i6KDh1xh027whhCa2o8='

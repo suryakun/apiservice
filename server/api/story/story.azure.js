b6289e69a117182418db831b762d8d58b0258216
surya <surya@localhost.localdomain>
2016-03-19 13:36:01 +0700

    fixing nodemailer

--- server/api/story/story.azure.js
+++ server/api/story/story.azure.js
@@ -1,4 +1,5 @@
 var outlook = require('node-outlook');
+var nodemailer = require("nodemailer");
 
 exports.createCalendar = function (email, event) {
     // Set the API endpoint to use the v2.0 endpoint
@@ -61,23 +62,35 @@ exports.getEvents = function (email) {
 }
 
 exports.sendMail = function (to, text) {
-    var server  = email.server.connect({
-       user:    "azhararraniry@hotmail.com", 
-       password:"Pa55word!*", 
-       host:    "smtp-mail.outlook.com", 
-       tls: {ciphers: "SSLv3"}
-    });
 
-    var message = {
-       from:    "Azhar <azhararraniry@hotmail.com>", 
-       to:      to,
-       subject: "7pagi notification",
-       attachment: 
-       [
-          {data:'<html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /><title>7pagi - Info</title><meta name="viewport" content="width=device-width, initial-scale=1.0"/></head><body style="margin: 0; padding: 0;"><table bgcolor="#f6f8fa" border="0" cellpadding="0" cellspacing="0" width="100%" style="font-family: \'Roboto\',\'Helvetica Neue\', Helvetica, Arial, sans-serif; max-width: 600px;margin: 10px auto"><tr><td bgcolor="#f6f8fa" style="text-align: center; padding: 20px 0;"><img src="web.7pagi.com:8080/assets/img/logo-small.png" alt="logo" /></td></tr><tr><td style="padding: 0 10px; color: #505458;"><table bgcolor="#ffffff" cellpadding="0" cellspacing="0" style="border-radius: 5px;"><tr><td style="padding: 20px"><table border="0" cellpadding="0" cellspacing="0" width="100%"><tr><td rowspan="2" style="width: 60px; text-align: center"><img width="35" height="35" style="border-radius: 50%;" src="'+ text.avatar + '" alt="profile"> </td><td><span style="font-size: 18px; font-weight: bold; display: block"> '+ text.author +' </span></td></tr><tr><td style="font-size: 13px;line-height: 11px;color: #B3C1C2;">Teacher</td></tr></table></td></tr><tr><td colspan="2" style="padding: 10px 20px 20px; line-height: 22px; font-size: 16px;">'+ text.description +'</td></tr></table></td></tr><tr><td bgcolor="#f6f8fa" style="padding:70px 20px 20px; text-align: center;color: #888888; font-size: 11px;">Copyright &copy; 2016 7Pagi Digital Edumedia</td></tr></table></body></html>', alternative:true}
-       ]
-    };
+  // create reusable transport method (opens pool of SMTP connections)
+  var smtpTransport = nodemailer.createTransport("SMTP",{
+      service: "Office365",
+      host: "smtp.office365.com",
+      tls: true,
+      port:587,
+      auth: {
+          user: "halo@7pagi.com",
+          pass: "7Pagi123"
+      }
+  });
 
-    // send the message and get a callback with an error or details of the message that was sent
-    server.send(message, function(err, message) { console.log(err || message); });
+  // setup e-mail data with unicode symbols
+  var mailOptions = {
+      from: "7Pagi <halo@7pagi.com>", // sender address
+      to: to, // list of receivers
+      subject: "7Pagi Notification", // Subject line
+      html: '<html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /><title>7pagi - Info</title><meta name="viewport" content="width=device-width, initial-scale=1.0"/></head><body style="margin: 0; padding: 0;"><table bgcolor="#f6f8fa" border="0" cellpadding="0" cellspacing="0" width="100%" style="font-family: \'Roboto\',\'Helvetica Neue\', Helvetica, Arial, sans-serif; max-width: 600px;margin: 10px auto"><tr><td bgcolor="#f6f8fa" style="text-align: center; padding: 20px 0;"><img src="web.7pagi.com:8080/assets/img/logo-small.png" alt="logo" /></td></tr><tr><td style="padding: 0 10px; color: #505458;"><table bgcolor="#ffffff" cellpadding="0" cellspacing="0" style="border-radius: 5px;"><tr><td style="padding: 20px"><table border="0" cellpadding="0" cellspacing="0" width="100%"><tr><td rowspan="2" style="width: 60px; text-align: center"><img width="35" height="35" style="border-radius: 50%;" src="'+ text.avatar + '" alt="profile"> </td><td><span style="font-size: 18px; font-weight: bold; display: block"> '+ text.author +' </span></td></tr><tr><td style="font-size: 13px;line-height: 11px;color: #B3C1C2;">Teacher</td></tr></table></td></tr><tr><td colspan="2" style="padding: 10px 20px 20px; line-height: 22px; font-size: 16px;">'+ text.description +'</td></tr></table></td></tr><tr><td bgcolor="#f6f8fa" style="padding:70px 20px 20px; text-align: center;color: #888888; font-size: 11px;">Copyright &copy; 2016 7Pagi Digital Edumedia</td></tr></table></body></html>' // html body
+  }
+
+  // send mail with defined transport object
+  smtpTransport.sendMail(mailOptions, function(error, response){
+      if(error){
+          console.log(error);
+      }else{
+          console.log("Message sent: " + response.message);
+      }
+      // if you don't want to use this transport object anymore, uncomment following line
+      //smtpTransport.close(); // shut down the connection pool, no more messages
+  });
 }

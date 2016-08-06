725276f735e10f8c913a8293ccc88bdd4c8a75bc
Surya <surya.ramshere@gmail.com>
2016-08-06 15:27:29 +0700

    student controller

--- client/app/student/student.controller.js
+++ client/app/student/student.controller.js
@@ -1,6 +1,12 @@
 'use strict';
 angular.module('roomApp').controller('StudentCtrl', ['$scope', 'myClassesHttp', '$http', function($scope, myClassesHttp, $http) {
     $scope.myClasses = [myClassesHttp.data];
+    var schoolId = $scope.myClasses._school._id;
+    var otherClass = $http.get('/api/schools/get-class-by-school-id/' + schoolId);
+    otherClass.then(function (response) {
+        console.log(response);
+        $scope.myClasses = response;
+    })
     $scope.selectedClass = $scope.myClasses[0];
     $scope.students = [];
     var getData = function() {
